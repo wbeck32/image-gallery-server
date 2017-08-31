0cc7079c3abdb0dcd5020d274e2e6a651bffc8fe
Wendy Beck <webeck@gmail.com>
2017-08-30 22:54:07 -0700

    all tests are passing!

--- src/tests/unit.tests.js
+++ src/tests/unit.tests.js
@@ -3,46 +3,48 @@ const { assert } = require('chai');
 const photos = require('../helpers/imageurls.json');
 
 describe('Image model tests', () => {
-  beforeEach(() => {
-
-
-  })
-  const testPhoto = new Image({
+  const testPhotoInvalid = new Image({
     url:
       'https://www.flickr.com/photos/' + photos[0].owner + '/' + photos[0].id,
     alt: 'alt text',
     description: 'description'
   });
 
-  it('image model fails validation', async () => {
-    const failValidate = await testPhoto.validate().catch(() => {
-        throw (new Error('Validation errors'),
-        ({ errors }) => {
-          // assert.equal();
-          // assert.equal();
-        });
-      });
-      console.log(failValidate);
+  const testPhotoValid = new Image({
+    title: 'this is a valid image',
+    url:
+      'https://www.flickr.com/photos/' + photos[1].owner + '/' + photos[1].id,
+    alt: 'alt text',
+    description: 'description'
+  });
 
-  }),
-    it('image model passes validation', () => {
-      const testPhoto = new Image({
-        title: photos[0].title,
-        url: 'https://www.flickr.com/photos/' + photos[0].owner + '/' + photos[0].id,
-        alt: 'alt text',
-        description: 'description'
+  it('image model fails validation', async () => {
+    const failValidate = await testPhotoInvalid
+      .validate(response => {
+        // console.log('fail: ', 'name: ',response.errors.title.name, 'message: ',response.errors.title.message);
+
+        return response;
+      })
+      .catch(response => {
+        assert.equal(response.errors.title.name, 'ValidatorError');
+        assert.equal(
+          response.errors.title.message,
+          'Path `title` is required.'
+        );
+        ({ errors }) => {
+          console.log('errors: ', errors);
+        };
       });
-      return testPhoto
-        .validate()
-        .then(() => {
-          // console.log('unit test: ', testPhoto);
+  }),
+    it('image model passes validation', async () => {
+      const passValidate = await testPhotoValid
+        .validate(response => {
+          assert.equal(null, response);
         })
         .catch(() => {
           throw (new Error('Validation errors'),
           ({ errors }) => {
-            console.log(errors);
-            // assert.equal();
-            // assert.equal();
+            console.log('errors: ', errors);
           });
         });
     });
